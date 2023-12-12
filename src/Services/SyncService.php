d5acb972e47a8f9f7d4964b61fbd07c0c3facc0e
Mollsoft <info@mollsoft.com>
2023-12-12 09:15:19 +0300

    Deposit WebHooks without command

--- src/Services/SyncService.php
+++ src/Services/SyncService.php
@@ -3,21 +3,29 @@
 namespace Mollsoft\LaravelBitcoinModule\Services;
 
 use Decimal\Decimal;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Log;
 use Mollsoft\LaravelBitcoinModule\BitcoindRpcApi;
 use Mollsoft\LaravelBitcoinModule\Models\BitcoinDeposit;
 use Mollsoft\LaravelBitcoinModule\Models\BitcoinWallet;
+use Mollsoft\LaravelBitcoinModule\WebhookHandlers\WebhookHandlerInterface;
 
 class SyncService
 {
     protected readonly BitcoindRpcApi $api;
+    protected readonly WebhookHandlerInterface $webhookHandler;
 
     /** @var BitcoinDeposit[] */
     protected array $newDeposits = [];
 
     public function __construct(protected readonly BitcoinWallet $wallet) {
         $this->api = $this->wallet->node->api();
+
+        /** @var class-string<WebhookHandlerInterface> $model */
+        $model = config('bitcoin.webhook_handler');
+        $this->webhookHandler = App::make($model);
     }
 
     public function run(): void
@@ -102,8 +110,8 @@ class SyncService
                 'wallet_id' => $this->wallet->id,
                 'amount' => new Decimal((string)$item['amount']),
                 'block_height' => $item['blockheight'] ?? null,
-                'confirmations' => $item['confirmations'],
                 'time_at' => Date::createFromTimestamp($item['time']),
+                'confirmations' => $item['confirmations'],
             ]);
 
             if ($deposit?->wasRecentlyCreated) {
@@ -117,9 +125,12 @@ class SyncService
     protected function executeWebhooks(): self
     {
         foreach ($this->newDeposits as $deposit) {
-            Artisan::call('bitcoin:webhook', [
-                'deposit_id' => $deposit->id,
-            ]);
+            try {
+                $this->webhookHandler->handle($deposit->wallet, $deposit->address, $deposit);
+            }
+            catch(\Exception $e) {
+                Log::error('Bitcoin WebHook for deposit '.$deposit->id.' - '.$e->getMessage());
+            }
         }
 
         return $this;
